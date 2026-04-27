687a0caa0633c892e3638d7da40413abc4086d0a
shams al labib <samslabib19@gmail.com>
2026-04-27 22:21:45 +0600

    73.1 finished

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "600", "800"],
@@ -13,7 +15,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.className}  antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <header className="py-2 w-11/12 mx-auto">
+          <Navbar />
+        </header>
+        <main className="py-2 w-11/12 mx-auto">{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
